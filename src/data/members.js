9c528d05eda543082299906301fa0817d7f84af5
ElaineShu0312 <elaineshu0755@gmail.com>
2024-02-04 21:55:11 -0800

    added headshots, except one

--- src/data/members.js
+++ src/data/members.js
@@ -3,21 +3,21 @@ export const members = [
     imagePath: require("../assets/Images/Headshots/Elaine Shu.jpeg"),
     name: "Elaine Shu",
     title: "President",
-    url: "https://cal.com/elaine-shu/15min"
+    url: "https://cal.com/elaine-shu/15min",
   },
 
   {
     imagePath: require("../assets/Images/Headshots/Aidan Sunbury.png"),
     name: "Aidan Sunbury",
     title: "Technical Director",
-    url: "https://cal.com/aidan-sunbury"
+    url: "https://cal.com/aidan-sunbury",
   },
 
   {
     imagePath: require("../assets/Images/Headshots/Andrea Lou.JPG"),
     name: "Andrea Lou",
     title: "Treasurer",
-    url: null
+    url: null,
   },
 
   {
@@ -31,125 +31,163 @@ export const members = [
     imagePath: require("../assets/Images/Headshots/Cameron Leung.jpg"),
     name: "Cameron Leung",
     title: "Marketing Director",
-    url: "https://cal.com/cameron-leung-a1jvxg/codifycoffeechat"
+    url: "https://cal.com/cameron-leung-a1jvxg/codifycoffeechat",
   },
 
   {
     imagePath: require("../assets/Images/Headshots/Rayna Bhattacharyya.jpg"),
     name: "Rayna Bhattacharyya",
     title: "Project Operations Director",
-    url: "https://cal.com/rayna-bhattacharyya/15min"
+    url: "https://cal.com/rayna-bhattacharyya/15min",
   },
 
   {
     imagePath: require("../assets/Images/Headshots/Kyle Casbon.JPG"),
     name: "Kyle Casbon",
     title: "Project Lead",
-    url: "https://cal.com/kyle-casbon/15min"
+    url: "https://cal.com/kyle-casbon/15min",
   },
   {
     imagePath: require("../assets/Images/Headshots/Cady He.jpeg"),
     name: "Cady He",
     title: "Project Lead",
-    url: "https://cal.com/cadyhe/30min?date=2024-01-15&month=2024-01"
+    url: "https://cal.com/cadyhe/30min?date=2024-01-15&month=2024-01",
   },
   {
     imagePath: require("../assets/Images/Headshots/Anshul Jambula.jpg"),
     name: "Anshul Jambula",
     title: "Project Lead",
-    url: null
+    url: null,
   },
 
   {
     imagePath: require("../assets/Images/Headshots/Paul Woo.png"),
     name: "Paul Woo",
     title: "Project Lead",
-    url: null
+    url: null,
   },
 
   {
     imagePath: require("../assets/Images/Headshots/Stacey Lei.jpg"),
     name: "Stacey Lei",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Saloni Khule.png"),
     name: "Saloni Khule",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Michael West.jpeg"),
     name: "Michael West",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Alivia Ding.jpg"),
     name: "Alivia Ding",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Samar Farooqi.jpg"),
     name: "Samar Farooqi",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Angela He.jpg"),
     name: "Angela He",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Chetan Goenka.jpg"),
     name: "Chetan Goenka",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Hiro Xiangyuan.jpeg"),
     name: "Hiro Xiangyuan",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Vrushank Prakash.JPG"),
     name: "Vrushank Prakash",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Keon Soltani.jpeg"),
     name: "Keon Soltani",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/David Vu.jpeg"),
     name: "David Vu",
     title: "Developer",
-    url: null
+    url: null,
   },
   {
-    imagePath: require("../assets/Images/Headshots/Alex Chow.JPG"),
-    name: "Alex Chow",
+    imagePath: require("../assets/Images/Headshots/Henry Tsai.jpg"),
+    name: "Henry Tsai",
     title: "Developer",
-    url: null
+    url: null,
   },
+  {
+    imagePath: require("../assets/Images/Headshots/Kinjal Govil.png"),
+    name: "Kinjal Govil",
+    title: "Developer",
+    url: null,
+  },
+  {
+    imagePath: require("../assets/Images/Headshots/Alif Satyawan.png"),
+    name: "Alif Satyawan",
+    title: "Developer",
+    url: null,
+  },
+  {
+    imagePath: require("../assets/Images/Headshots/Faisal Aldurayhim.jpeg"),
+    name: "Faisal Aldurayhim",
+    title: "Developer",
+    url: null,
+  },
+  {
+    imagePath: require("../assets/Images/Headshots/Rohan Kudchadker.jpeg"),
+    name: "Rohan Kudchadker",
+    title: "Developer",
+    url: null,
+  },
+  {
+    imagePath: require("../assets/Images/Headshots/Joshua Chen.jpg"),
+    name: "Joshua Chen",
+    title: "Developer",
+    url: null,
+  },
+  //TODO: Add Chloe Lin :-)
+  {
+    imagePath: null,
+    name: null,
+    title: null,
+    url: null,
+  },
+
   {
     imagePath: require("../assets/Images/Headshots/Tiffany Huang.jpg"),
     name: "Tiffany Huang",
     title: "Designer",
-    url: null
+    url: null,
   },
   {
     imagePath: require("../assets/Images/Headshots/Allena Oglivie.JPG"),
     name: "Allena Oglivie",
     title: "Designer",
-    url: "https://cal.com/allena-oglivie-afog"
+    url: "https://cal.com/allena-oglivie-afog",
   },
 ];
 
