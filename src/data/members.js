8a958caa9d98200ffdcdb2e44f3897578f540285
aidansunbury <aidansunbury@gmail.com>
2024-08-28 01:20:33 -0700

    updated images

--- src/data/members.js
+++ src/data/members.js
@@ -1,191 +1,135 @@
 export const members = [
-  {
-    imagePath: require("../assets/Images/Headshots/Elaine Shu.jpeg"),
-    name: "Elaine Shu",
-    title: "President",
-    url: "https://cal.com/elaine-shu/15min",
-  },
+	{
+		imagePath: require("../assets/Images/Headshots/Kyle Casbon.JPG"),
+		name: "Kyle Casbon",
+		title: "Project Lead",
+		url: "https://cal.com/kyle-casbon/15min",
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Aidan Sunbury.png"),
+		name: "Aidan Sunbury",
+		title: "Projects Director",
+		url: "https://cal.com/aidan-sunbury",
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Cameron Leung.jpg"),
+		name: "Cameron Leung",
+		title: "Marketing Director",
+		url: "https://cal.com/cameron-leung-a1jvxg/codifycoffeechat",
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Cady He.jpeg"),
+		name: "Cady He",
+		title: "Education Director",
+		url: "https://cal.com/cadyhe/30min",
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Niki Parker.jpeg"),
+		name: "Niki Parker",
+		title: "External Director",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Rayna Bhattacharyya.jpg"),
+		name: "Rayna Bhattacharyya",
+		title: "Project Lead",
+		url: "https://cal.com/rayna-bhattacharyya/15min",
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Henry Tsai.jpg"),
+		name: "Henry Tsai",
+		title: "Project Lead",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Samar Farooqi.jpg"),
+		name: "Samar Farooqi",
+		title: "Project Lead",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Shayan Arasan.jpg"),
+		name: "Shayan Arasan",
+		title: "Project Lead",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/David Vu.jpeg"),
+		name: "David Vu",
+		title: "Project Lead",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Saloni Khule.png"),
+		name: "Saloni Khule",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Michael West.jpeg"),
+		name: "Michael West",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Alivia Ding.jpg"),
+		name: "Alivia Ding",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Angela He.jpg"),
+		name: "Angela He",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Chetan Goenka.jpg"),
+		name: "Chetan Goenka",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Kinjal Govil.png"),
+		name: "Kinjal Govil",
+		title: "Developer",
+		url: null,
+	},
 
-  {
-    imagePath: require("../assets/Images/Headshots/Aidan Sunbury.png"),
-    name: "Aidan Sunbury",
-    title: "Technical Director",
-    url: "https://cal.com/aidan-sunbury",
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Andrea Lou.JPG"),
-    name: "Andrea Lou",
-    title: "Treasurer",
-    url: null,
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Krista Morales.jpeg"),
-    name: "Krista Morales",
-    title: "Culture Director",
-    url: "https://cal.com/krista-espinosa-morales-rz7s9y/15min",
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Cameron Leung.jpg"),
-    name: "Cameron Leung",
-    title: "Marketing Director",
-    url: "https://cal.com/cameron-leung-a1jvxg/codifycoffeechat",
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Rayna Bhattacharyya.jpg"),
-    name: "Rayna Bhattacharyya",
-    title: "Project Operations Director",
-    url: "https://cal.com/rayna-bhattacharyya/15min",
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Kyle Casbon.JPG"),
-    name: "Kyle Casbon",
-    title: "Project Lead",
-    url: "https://cal.com/kyle-casbon/15min",
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Cady He.jpeg"),
-    name: "Cady He",
-    title: "Project Lead",
-    url: "https://cal.com/cadyhe/30min?date=2024-01-15&month=2024-01",
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Anshul Jambula.jpg"),
-    name: "Anshul Jambula",
-    title: "Project Lead",
-    url: null,
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Paul Woo.png"),
-    name: "Paul Woo",
-    title: "Project Lead",
-    url: null,
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Stacey Lei.jpg"),
-    name: "Stacey Lei",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Saloni Khule.png"),
-    name: "Saloni Khule",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Michael West.jpeg"),
-    name: "Michael West",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Alivia Ding.jpg"),
-    name: "Alivia Ding",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Samar Farooqi.jpg"),
-    name: "Samar Farooqi",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Angela He.jpg"),
-    name: "Angela He",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Chetan Goenka.jpg"),
-    name: "Chetan Goenka",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Hiro Xiangyuan.jpeg"),
-    name: "Hiro Xiangyuan",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Vrushank Prakash.JPG"),
-    name: "Vrushank Prakash",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Keon Soltani.jpeg"),
-    name: "Keon Soltani",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/David Vu.jpeg"),
-    name: "David Vu",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Henry Tsai.jpg"),
-    name: "Henry Tsai",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Kinjal Govil.png"),
-    name: "Kinjal Govil",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Alif Satyawan.png"),
-    name: "Alif Satyawan",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Faisal Aldurayhim.jpeg"),
-    name: "Faisal Aldurayhim",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Rohan Kudchadker.jpeg"),
-    name: "Rohan Kudchadker",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Joshua Chen.jpg"),
-    name: "Joshua Chen",
-    title: "Developer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Chloe Lin.jpeg"),
-    name: "Chloe Lin",
-    title: "Developer",
-    url: null,
-  },
-
-  {
-    imagePath: require("../assets/Images/Headshots/Tiffany Huang.jpg"),
-    name: "Tiffany Huang",
-    title: "Designer",
-    url: null,
-  },
-  {
-    imagePath: require("../assets/Images/Headshots/Allena Oglivie.JPG"),
-    name: "Allena Oglivie",
-    title: "Designer",
-    url: "https://cal.com/allena-oglivie-afog",
-  },
+	{
+		imagePath: require("../assets/Images/Headshots/Rohan Kudchadker.jpeg"),
+		name: "Rohan Kudchadker",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Joshua Chen.jpg"),
+		name: "Joshua Chen",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Chloe Lin.jpeg"),
+		name: "Chloe Lin",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Emiliano Garcia.jpeg"),
+		name: "Emiliano Garcia",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Aiji Li.jpeg"),
+		name: "Aiji Li",
+		title: "Developer",
+		url: null,
+	},
+	{
+		imagePath: require("../assets/Images/Headshots/Nassib Chaya.jpeg"),
+		name: "Nassib Chaya",
+		title: "Developer",
+		url: null,
+	},
 ];
--- src/data/timelineData.js
+++ src/data/timelineData.js
@@ -2,73 +2,65 @@
 // subHeadings are optional
 
 export const timelineData = [
-  // /* NEED TO UPDATE TIMELINE DATA
-  {
-    title: "Applications Open",
-    description: (
-      <span>
-        Looking for passionate developers or UI/UX designers!
-        <br></br>
-        {/* <a
+	// /* NEED TO UPDATE TIMELINE DATA
+	{
+		title: "Applications Open",
+		description: (
+			<span>
+				Looking for passionate developers!
+				<br></br>
+				{/* <a
           href="https://forms.gle/GJEixD4AFTwkWrbf6"
           target="_blank"
           rel="noreferrer"
         >
           Application Form
         </a> */}
-      </span>
-    ),
-    date: "1/16/24",
-  },
-  {
-    title: "Tabling",
-    description: "Come talk to us on the glade to learn more about Codify.",
-    date: "1/16/24-1/23/24",
-  },
-  {
-    title: "Infosession",
-    description:
-      "Learn about current projects, club culture, and mingle with current Codify members at our info session. An initial 45 minute presentation will be followed by a Q & A period and a mixer with members.",
-    date: "1/23/24",
-    subHeading: "Time: 8-10 PM, Location: Dwinelle 179",
-  },
-  {
-    title: "Game Night Social",
-    description:
-      "Come to our game night to meet current Codify members, play games, eat food, and have fun!",
-    date: "1/25/24",
-    subHeading: "Time: 8-10 PM, Location: Dwinelle 234",
-  },
-
-  {
-    title: "Final Application Deadline",
-    description:
-      "All applications are due by 11:59 PM. If you have any last minute difficulties submitting, please email us at codify.berkeley@gmail.com",
-    date: "1/26/24",
-  },
-  {
-    title: "Interviews",
-    description:
-      "After submitting an application, you may be invited to schedule an interview with us. More details will be released then. ",
-    date: "1/28/23-1/29/24",
-  },
-  {
-    title: "Decisions Released",
-    description: "Check your email to see if you've been accepted!",
-    date: "1/29/24",
-  },
-
-  // {
-  //   title: "Education Program Begins",
-  //   description:
-  //     "Our education program teaches the fundamentals of full stack web and mobile development. Completing the entire program with a high enough grade guarantees acceptance into Codify for the following semester.",
-  //   date: "9/7/23",
-  // },
-  // {
-  //   title: "Final Application Decisions Released",
-  //   description:
-  //     "Check your email to see if you've been accepted! Every semester we receive many more applications than we can accept, so if you are not accepted, we encourage you to apply again next semester.",
-  //   date: "9/8/23",
-  // },
-  // */
+			</span>
+		),
+		date: "8/30/24",
+	},
+	{
+		title: "Tabling",
+		description: "Come talk to us on the glade to learn more about Codify.",
+		date: "8/29/24-9/3/24",
+	},
+	{
+		title: "Infosession",
+		description:
+			"Learn about current projects, club culture, and mingle with current Codify members at our info session. An initial 30 minute presentation will be followed by a Q & A period and a mixer with members.",
+		date: "9/3/24",
+		subHeading: "Time: 8-9 PM, Location: TBA",
+	},
+	{
+		title: "Interview Prep Workshop",
+		description:
+			"Come learn everything you need to know to succeed in the technical interview.",
+		date: "9/5/24",
+		subHeading: "Time: 8-9:30 PM, Location: TBA",
+	},
+	{
+		title: "Game Night Social",
+		description:
+			"Come to our game night to meet current Codify members, play games, eat food, and have fun!",
+		date: "9/6/24",
+		subHeading: "Time: 8-10 PM, Location: TBA",
+	},
+	{
+		title: "Final Application Deadline",
+		description:
+			"All applications are due by 11:59 PM. If you have any last minute difficulties submitting, please email us at codify.berkeley@gmail.com",
+		date: "9/6/24",
+	},
+	{
+		title: "Interviews",
+		description:
+			"After submitting an application, you may be invited to schedule an interview with us. More details will be released then. ",
+		date: "Labor Day Weekend",
+	},
+	{
+		title: "Decisions Released",
+		description: "Check your email to see if you've been accepted!",
+		date: "TBA",
+	},
 ];
