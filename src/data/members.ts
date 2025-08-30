422a6885be114da913234e5bb8a3a80160d1b31f
Arthur Sanchez <arthurs347251@gmail.com>
2025-08-30 16:52:30 -0700

    Updated members page, and headshots

--- src/data/members.ts
+++ src/data/members.ts
@@ -7,7 +7,7 @@ export type Member = {
 
 export const members: Member[] = [
 	{
-		imagePath: require("../assets/Images/Headshots/SP25_Arthur_Matthew_Sanchez.jpeg"),
+		imagePath: require("../assets/Images/Headshots/FA25_Arthur_Sanchez.jpg"),
 		name: "Arthur Sanchez",
 		title: "Co-President",
 		url: null,
@@ -24,6 +24,24 @@ export const members: Member[] = [
 		title: "Senior Advisor",
 		url: "https://cal.com/rayna-bhattacharyya/15min",
 	},
+    {
+        imagePath: require("../assets/Images/Headshots/FA25_Daniel_Smothers.jpg"),
+        name: "Daniel Smothers",
+        title: "Treasurer",
+        url: null,
+    },
+    {
+        imagePath: require("../assets/Images/Headshots/FA25_Vikram_Nandi.png"),
+        name: "Vikram Nandi",
+        title: "Treasurer",
+        url: null,
+    },
+    {
+        imagePath: require("../assets/Images/Headshots/FA25_Fernando_Martinez.jpeg"),
+        name: "Fernando Martinez",
+        title: "Education Director",
+        url: null,
+    },
 	{
 		imagePath: require("../assets/Images/Headshots/SP25_Rohan Kudchadker.jpg"),
 		name: "Rohan Kudchadker",
@@ -31,47 +49,41 @@ export const members: Member[] = [
 		url: null,
 	},
 	{
-		imagePath: require("../assets/Images/Headshots/Kinjal Govil.png"),
+		imagePath: require("../assets/Images/Headshots/FA25_Kinjal_Govil.jpg"),
 		name: "Kinjal Govil",
 		title: "Project Lead",
 		url: null,
 	},
 	{
-		imagePath: require("../assets/Images/Headshots/SP25_Trustin_Nguyen.jpg"),
-		name: "Trustin Nguyen",
-		title: "Project Lead",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Lakshay_Soin.png"),
+		imagePath: require("../assets/Images/Headshots/FA25_Lakshay_Soin.jpg"),
 		name: "Lakshay Soin",
 		title: "Project Lead",
 		url: null,
 	},
+    {
+        imagePath: require("../assets/Images/Headshots/SP25_Trustin_Nguyen.jpg"),
+        name: "Trustin Nguyen",
+        title: "Developer",
+        url: null,
+    },
+    {
+        imagePath: require("../assets/Images/Headshots/FA25_Emiliano_Cereceres.png"),
+        name: "Emiliano Cereceres",
+        title: "Developer",
+        url: null,
+    },
 	{
-		imagePath: require("../assets/Images/Headshots/Emiliano Garcia.jpeg"),
-		name: "Emiliano Garcia",
-		title: "Education: Content",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/Joshua Chen.jpg"),
-		name: "Joshua Chen",
-		title: "Senior Developer",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Rian_Puri.jpeg"),
+		imagePath: require("../assets/Images/Headshots/FA25_Rian_Puri.jpg"),
 		name: "Rian Puri",
 		title: "Developer",
 		url: null,
 	},
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Gabryel_Cher_Chin_Soh.jpg"),
-		name: "Gabryel Soh",
-		title: "Developer",
-		url: null,
-	},
+    {
+        imagePath: require("../assets/Images/Headshots/FA25_Benjamin_Smothers.jpg"),
+        name: "Benjamin Smothers",
+        title: "Developer",
+        url: null,
+    },
 	{
 		imagePath: require("../assets/Images/Headshots/SP25_Neel_Gajare.jpg"),
 		name: "Neel Gajare",
@@ -79,27 +91,33 @@ export const members: Member[] = [
 		url: null,
 	},
 	{
-		imagePath: require("../assets/Images/Headshots/SP25_Sahil_Kumawat.jpg"),
-		name: "Sahil Kumawat",
+		imagePath: require("../assets/Images/Headshots/SP25_Christopher_I_Kim.png"),
+		name: "Christopher Kim",
 		title: "Developer",
 		url: null,
 	},
 	{
-		imagePath: require("../assets/Images/Headshots/SP25_Christopher_I_Kim.png"),
-		name: "Christopher Kim",
-		title: "Student",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Umesh_Gopi.jpeg"),
+		imagePath: require("../assets/Images/Headshots/FA25_Umesh_Gopi.png"),
 		name: "Umesh Gopi",
-		title: "Student",
+		title: "Developer",
 		url: null,
 	},
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Vikram_Nandi.png"),
-		name: "Vikram Nandi",
-		title: "Student",
-		url: null,
-	},
+    {
+        imagePath: require("../assets/Images/Headshots/FA25_Samar_Farooqi.jpg"),
+        name: "Samar Farooqi",
+        title: "General Member",
+        url: null,
+    },
+    {
+        imagePath: require("../assets/Images/Headshots/SP25_Angela_He.png"),
+        name: "Angela He",
+        title: "General Member",
+        url: null,
+    },
+    {
+        imagePath: require("../assets/Images/Headshots/Joshua Chen.jpg"),
+        name: "Umesh Gopi",
+        title: "General Member",
+        url: null,
+    },
 ];
