c2f3b8278d27c290ec18955a137d28f847a9b95d
Arthur Sanchez <arthurs347251@gmail.com>
2025-08-30 00:14:18 -0700

    Refactored MemberCard so that flippable functionality is optional and can be set manually. Also improved styling.

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import LocalCafeIcon from "@mui/icons-material/LocalCafe";
 import './MemberCard.css'; // Import the regular CSS file
 
+const FLIPPABLE = false; // Set to true to enable flip card functionality
+
 interface MemberCardProps {
   imagePath: string;
   name: string;
@@ -11,9 +13,9 @@ interface MemberCardProps {
 }
 
 const MemberCard: React.FC<MemberCardProps> = ({ imagePath, name, title, url, backContent }) => {
-  return (
-    <div className="flex-col h-90 w-60 shadow-lg overflow-hidden m-3">
-      <div className="flipCard">
+    return (
+    <div className="flex-col h-90 w-60 shadow-lg overflow-hidden m-3 hover:shadow-lg hover:-translate-y-3 duration-300 z-2 hover:cursor-pointer rounded-md">
+      <div className={FLIPPABLE ? "flipCard" : "regCard"}>
         <div className="flipCardInner">
           <div className="flipCardFront">
             <img className="image h-60 w-full object-cover p-3" src={imagePath} alt={name} />
@@ -29,9 +31,9 @@ const MemberCard: React.FC<MemberCardProps> = ({ imagePath, name, title, url, ba
               </div>
             </div>
           </div>
-          <div className="flipCardBack">
-            <p>{backContent}</p>
-          </div>
+            <div className="flipCardBack">
+                <p>{backContent}</p>
+            </div>
         </div>
       </div>
     </div>
