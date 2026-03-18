0bb1fa425d3dbc6a9303d5870e300b9e0a13a0ca
Benjamin Smothers <benjaminsmothers@berkeley.edu>
2026-03-18 16:34:23 -0700

    done with headshots

--- src/data/members.ts
+++ src/data/members.ts
@@ -1,123 +1,198 @@
 export type Member = {
-    name: string;
-    title: string;
-    imagePath: string;
-    url: string | null;
+   name: string;
+   title: string;
+   imagePath: string;
+   url: string | null;
 }
 
+
 export const members: Member[] = [
-	{
-		imagePath: require("../assets/Images/Headshots/FA25_Arthur_Sanchez.jpg"),
-		name: "Arthur Sanchez",
-		title: "Co-President",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/Michael West.jpeg"),
-		name: "Michael West",
-		title: "Co-President",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/Rayna Bhattacharyya.jpg"),
-		name: "Rayna Bhattacharyya",
-		title: "Senior Advisor",
-		url: "https://cal.com/rayna-bhattacharyya/15min",
-	},
-    {
-        imagePath: require("../assets/Images/Headshots/FA25_Daniel_Smothers.jpg"),
-        name: "Daniel Smothers",
-        title: "Treasurer",
-        url: null,
-    },
-    {
-        imagePath: require("../assets/Images/Headshots/FA25_Vikram_Nandi.png"),
-        name: "Vikram Nandi",
-        title: "Treasurer",
-        url: null,
-    },
-    {
-        imagePath: require("../assets/Images/Headshots/FA25_Fernando_Martinez.jpeg"),
-        name: "Fernando Martinez",
-        title: "Education Director",
-        url: null,
-    },
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Rohan Kudchadker.jpg"),
-		name: "Rohan Kudchadker",
-		title: "Project Lead",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/FA25_Kinjal_Govil.jpg"),
-		name: "Kinjal Govil",
-		title: "Project Lead",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/FA25_Lakshay_Soin.jpg"),
-		name: "Lakshay Soin",
-		title: "Project Lead",
-		url: null,
-	},
-    {
-        imagePath: require("../assets/Images/Headshots/SP25_Trustin_Nguyen.jpg"),
-        name: "Trustin Nguyen",
-        title: "Developer",
-        url: null,
-    },
-    {
-        imagePath: require("../assets/Images/Headshots/FA25_Emiliano_Cereceres.png"),
-        name: "Emiliano Cereceres",
-        title: "Developer",
-        url: null,
-    },
-	{
-		imagePath: require("../assets/Images/Headshots/FA25_Rian_Puri.jpg"),
-		name: "Rian Puri",
-		title: "Developer",
-		url: null,
-	},
-    {
-        imagePath: require("../assets/Images/Headshots/FA25_Benjamin_Smothers.jpg"),
-        name: "Benjamin Smothers",
-        title: "Developer",
-        url: null,
-    },
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Neel_Gajare.jpg"),
-		name: "Neel Gajare",
-		title: "Developer",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/SP25_Christopher_I_Kim.png"),
-		name: "Christopher Kim",
-		title: "Developer",
-		url: null,
-	},
-	{
-		imagePath: require("../assets/Images/Headshots/FA25_Umesh_Gopi.png"),
-		name: "Umesh Gopi",
-		title: "Developer",
-		url: null,
-	},
-    {
-        imagePath: require("../assets/Images/Headshots/FA25_Samar_Farooqi.jpg"),
-        name: "Samar Farooqi",
-        title: "General Member",
-        url: null,
-    },
-    {
-        imagePath: require("../assets/Images/Headshots/SP25_Angela_He.png"),
-        name: "Angela He",
-        title: "General Member",
-        url: null,
-    },
-    {
-        imagePath: require("../assets/Images/Headshots/Joshua Chen.jpg"),
-        name: "Joshua Chen",
-        title: "General Member",
-        url: null,
-    },
+   {
+      imagePath: require("../assets/Images/Headshots/FA25_Arthur_Sanchez.jpg"),
+      name: "Arthur Sanchez",
+      title: "Co-President",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/Michael West.jpeg"),
+      name: "Michael West",
+      title: "Co-President",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/Rayna Bhattacharyya.jpg"),
+      name: "Rayna Bhattacharyya",
+      title: "Senior Advisor",
+      url: "https://cal.com/rayna-bhattacharyya/15min",
+   },
+   {
+       imagePath: require("../assets/Images/Headshots/FA25_Daniel_Smothers.jpg"),
+       name: "Daniel Smothers",
+       title: "Treasurer, Project Lead, Marketing",
+       url: null,
+   },
+   {
+       imagePath: require("../assets/Images/Headshots/SP26Vikram.jpg"),
+       name: "Vikram Nandi",
+       title: "Treasurer, Education Co-Director",
+       url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26CarlosHerrera.jpg"),
+      name: "Carlos Herrera",
+      title: "Culture Director",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP25_Christopher_I_Kim.png"),
+      name: "Christopher Kim",
+      title: "Marketing",
+      url: null,
+   },
+   {
+       imagePath: require("../assets/Images/Headshots/FA25_Fernando_Martinez.jpeg"),
+       name: "Fernando Martinez",
+       title: "Outreach",
+       url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/blank.jpeg"),
+      name: "Lleyton Okada-Alonzo",
+      title: "Outreach",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26ChristopherWang.jpg"),
+      name: "Christopher Wang",
+      title: "Outreach",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26YinuoChen.jpg"),
+      name: "Yinuo Chen",
+      title: "Project Lead",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/blank.jpeg"),
+      name: "Parsa Faraji",
+      title: "Project Lead",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26LakshaySoin.png"),
+      name: "Lakshay Soin",
+      title: "Project Lead",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26KarthikSubramanian.png"),
+      name: "Karthik Subramanian",
+      title: "Education Co-Director",
+      url: null,
+   },
+   {
+       imagePath: require("../assets/Images/Headshots/SP26Benjamin.jpg"),
+       name: "Benjamin Smothers",
+       title: "Developer",
+       url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26SiriwathEdwards.jpg"),
+      name: "Siriwath Edwards",
+      title: "Developer",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26MuEnShen.jpg"),
+      name: "Chris Shen",
+      title: "Developer",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26GabrielSandoval.jpg"),
+      name: "Gabriel Sandoval",
+      title: "Developer",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26RonitMathur.png"),
+      name: "Ronit Mathur",
+      title: "Developer",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26AliceWu.jpg"),
+      name: "Alice Wu",
+      title: "Developer",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26GautamKathiravan.jpg"),
+      name: "Gautam Kathiravan",
+      title: "Developer",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP26LavanyaKrishna.jpg"),
+      name: "Lavanya Krishna",
+      title: "Developer",
+      url: null,
+   },
+   {
+       imagePath: require("../assets/Images/Headshots/FA25_Samar_Farooqi.jpg"),
+       name: "Samar Farooqi",
+       title: "General Member",
+       url: null,
+   },
+   {
+       imagePath: require("../assets/Images/Headshots/SP26AngelaHe.jpg"),
+       name: "Angela He",
+       title: "General Member",
+       url: null,
+   },
+   {
+       imagePath: require("../assets/Images/Headshots/Joshua Chen.jpg"),
+       name: "Joshua Chen",
+       title: "General Member",
+       url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP25_Rohan Kudchadker.jpg"),
+      name: "Rohan Kudchadker",
+      title: "General Member",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP25_Trustin_Nguyen.jpg"),
+      name: "Trustin Nguyen",
+      title: "General Member",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/FA25_Rian_Puri.jpg"),
+      name: "Rian Puri",
+      title: "General Member",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/FA25_Emiliano_Cereceres.png"),
+      name: "Emiliano Cereceres",
+      title: "General Member",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/SP25_Neel_Gajare.jpg"),
+      name: "Neel Gajare",
+      title: "General Member",
+      url: null,
+   },
+   {
+      imagePath: require("../assets/Images/Headshots/Stacey Lei.jpg"),
+      name: "Stacy Lei",
+      title: "General Member",
+      url: null,
+   }
 ];
+
+
